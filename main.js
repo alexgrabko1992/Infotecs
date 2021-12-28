89873705c7b6a44b27ad1dc391c7b4b16d5feacb
Alex <alexgrabko1992@gmail.com>
2021-12-28 17:03:41 +0300

    complete table

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
-// alert("Hello");
-
 // Импортируем данные для таблицы из data.js
 
 import { data } from "./data.js";
 
 // Пишем лупер для того, чтобы они отобразились в таблице с уже готовой шапкой
-let yourChoice;
+
+let yourChoice; //переменная, в которую мы потом запишем id выбранной строчки
 
 for (let i = 0; i < data.length; i++) {
-  let tr = document.createElement("tr");
-  tr.id = i + 1;
+  let tr = document.createElement("tr"); // создаем строку
+  tr.id = i + 1; //присваиваем id для дальнейшей выборки
+  //создаем массив с нужными нам значениями из data.js, именем, фамилией, описанием и цветом глаз
   let array = [
     data[i].name.firstName,
     data[i].name.lastName,
     data[i].about,
     data[i].eyeColor,
   ];
+  // создаем еще один лупер, для присваивания значений в каждую ячейку таблицы
   for (let j = 0; j < array.length; j++) {
-    let td = document.createElement("td");
-    // td.classList.add(j + 1);
+    let td = document.createElement("td"); // создаем ячейку
     if (j === 2) {
-      td.classList.add("about");
+      td.classList.add("about"); // присваиваем класс ячейке с описанием, чтобы в дальнейшем ее урезать
     }
-    td.innerHTML = array[j];
-    // console.log(array[j]);
-    tr.appendChild(td);
+    td.innerHTML = array[j]; // присваиваем значение ячейке из массива, который создали ранее
+    tr.appendChild(td); // добавляем ячейку в элемент строки
   }
-  document.getElementById("dest").appendChild(tr);
+  document.getElementById("dest").appendChild(tr); //добавляем строку к элементу tbody
+  // создаем функцию, для выборки строк и создания формы
   tr.onclick = function () {
     const form = document.getElementById("form");
-    form.style.visibility = "visible";
+    form.style.visibility = "visible"; // делаем уже готовую форму видимой
+    // делаем так, чтобы при создании в поля инпута вставлялись исходные значения из таблицы
     const name = document.getElementById("name");
     name.value = array[0];
     last_name.value = array[1];
@@ -37,35 +38,31 @@ for (let i = 0; i < data.length; i++) {
     document.querySelector(
       `input[type='radio'][value=${array[3]}]`
     ).checked = true;
-    yourChoice = tr.id;
+    yourChoice = tr.id; // сохраняем в ранее созданную переменную значения id строки, которую мы изменяли
   };
 }
 
-// const tr = document.createElement("tr");
+// создаем редактируемую форму
 
-// const td = document.createElement("td");
+const button = document.querySelector("button"); // сохраняем объект DOM под названием button в переменную
 
-// td.textContent = data[0].name.firstName;
-// tr.appendChild(td);
-// document.getElementById("dest").appendChild(tr);
-
-const button = document.querySelector("button");
-
+// ставим слушатель на объект, который мы только что сохранили
 button.onclick = function () {
   const name = document.getElementById("name");
-  const lastName = document.getElementById("last_name");
+  const lastName = document.getElementById("last_name"); // сохраняем при нажатии данные, которые остались в input
   const about = document.getElementById("about");
 
-  const tr = document.getElementById(`${yourChoice}`);
+  const tr = document.getElementById(`${yourChoice}`); // находим строку к которой мы прежде обратились
 
   tr.childNodes[0].innerHTML = name.value;
-  tr.childNodes[1].innerHTML = lastName.value;
+  tr.childNodes[1].innerHTML = lastName.value; // при нажатии заменяем прежние данные сохраненными из input
   tr.childNodes[2].innerHTML = about.value;
 
+  // делаем специальный лупер для получения значений value из radio-элкментов
   const radio = document.getElementsByName("eye_color");
   for (let i = 0; i < radio.length; i++) {
     if (radio[i].checked) {
-      tr.childNodes[3].innerHTML = radio[i].value;
+      tr.childNodes[3].innerHTML = radio[i].value; // заменяем существующие, как мы делали это ранее
     }
   }
 };
--- sort.js
+++ sort.js
@@ -1,16 +1,23 @@
 // Функция для сортировки по алфавиту элементов таблицы
 
+// создаем слушатель для для прогрузки страницы
 document.addEventListener("DOMContentLoaded", () => {
+  // создаем функцию для сортировки элементов
   const getSort = ({ target }) => {
     const order = (target.dataset.order = -(target.dataset.order || -1));
     const index = [...target.parentNode.cells].indexOf(target);
-    const collator = new Intl.Collator(["en", "ru"], { numeric: true });
+
+    // создаем экземпляр конструктора сортировщиков для сравнения строк
+
+    const collator = new Intl.Collator(["en", "ru"], { numeric: true }); // прописываем параметры по которым будем сравнивать
     const comparator = (index, order) => (a, b) =>
       order *
       collator.compare(
-        a.children[index].innerHTML,
+        a.children[index].innerHTML, // функция для сравнения двух строк
         b.children[index].innerHTML
       );
+
+    // создаем лупер для добавления всех отсортированных строк в tbody
 
     for (const tBody of target.closest("table").tBodies)
       tBody.append(...[...tBody.rows].sort(comparator(index, order)));
@@ -19,9 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
       cell.classList.toggle("sorted", cell === target);
   };
 
-  document
-    .querySelectorAll(".table_sort thead")
-    .forEach((tableTH) =>
-      tableTH.addEventListener("click", () => getSort(event))
-    );
+  document.querySelectorAll(".table_sort thead").forEach(
+    (tableTH) => tableTH.addEventListener("click", () => getSort(event)) // слушатель для клика по элементу th, который вызывает фун-ию для сортировки
+  );
 });
